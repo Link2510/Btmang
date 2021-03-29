c6cce7df50b1f8a25b2ac65faff386c0455a074f
PHAM THONG <16dh110065@st.huflit.edu.vn>
2021-03-29 21:06:49 +0700

    Thêm bài 15, bài 16 chưa xong

--- BTthem15-RemoveACharacter/Program.cs
+++ BTthem15-RemoveACharacter/Program.cs
@@ -5,13 +5,34 @@ namespace BTthem15_RemoveACharacter
 {
     class Program
     {
+        public const int sizeOfArray = 10000;   
         static void Main(string[] args)
         {
             Console.Write("Nhap cau ban thich: ");
-            
+            string c = Console.ReadLine();
+            char[] a=new char[sizeOfArray];
+            for (int i = 0; i < c.Length; i++)
+            {
+                a[i] = c[i];
+            }
             Console.Write("Nhap vao vi tri ban muon xoa trong cau: ");
             int n = int.Parse(Console.ReadLine());
+            dCharacter(a, n);
             
+        }
+        static void dCharacter(char[] c, int n)
+        {
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (i == n)
+                {
+                    c[i] = (char)0;
+                }
+            }
+            foreach (char b in c)
+            {
+                Console.Write(b);
+            }
         }
     }
 }
--- BTthem16-Sanitiser/Program.cs
+++ BTthem16-Sanitiser/Program.cs
@@ -8,10 +8,30 @@ namespace BTthem16_Sanitiser
         static void Main(string[] args)
         {
             Console.WriteLine("Hay nhap chuoi cua ban vao: ");
-            char[] c = Console.ReadLine();
-            foreach (char item in c)
+            string tmp = Console.ReadLine();
+            char[] ch = new char[tmp.Length];
+            for (int i = 0; i < tmp.Length; i++)
             {
-                Console.WriteLine(item);
+                ch[i] = tmp[i];
+            }
+            deleteNumberic(ch);
+        }
+        static void deleteNumberic (char[] a) 
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if ((a[i] >= 0) && (a[i] <= 9))
+                {
+                    a[i] = (char)0;
+                }
+                else
+                {
+                    Console.WriteLine("Khong hieu");
+                }
+            }
+            foreach (char c in a)
+            {
+                Console.Write(c);
             }
         }
     }
