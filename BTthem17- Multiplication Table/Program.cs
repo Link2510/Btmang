98222e35e4a32a7a9ecbb22fe37105a5738cc056
PHAM THONG <16dh110065@st.huflit.edu.vn>
2021-03-30 20:43:30 +0700

    Add Multiplication table - Exercise 17

---
+++ BTthem17- Multiplication Table/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BTthem17__Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Multiplication Table from 1 to 10");
+            int[,] table = new int[10, 10];
+            for (int row = 0; row < 10; row++)
+            {
+                for (int column = 0; column < 10; column++)
+                {
+                    table[row, column] = (row + 1) * (column + 1);
+                    Console.Write("{0}*{1}={2}",row+1,column+1,table[row, column]);
+                    if (column < 9)
+                    {
+                        Console.Write("\t");
+                    }
+                    else
+                        Console.WriteLine();
+                }
+            }
+        }
+    }
+}
